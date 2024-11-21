72b3b9a4d7da0b87230612196c15796a9d71d057
Ashish0609 <ashishkanapuram@gmail.com>
2024-11-21 20:51:18 +0530

    Update Myclass.java

--- Myclass.java
+++ Myclass.java
@@ -1,7 +1,6 @@
 class MyClass {
   public static void main(String[] args) {
     int x, y, sum;
-    Scanner myObj = new Scanner(System.in); // Create a Scanner object
     System.out.println("Type a number:");
     x = myObj.nextInt(); // Read user input
 
