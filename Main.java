c6b1e3c3c4f5613a6d2bb5484ede796d8e7de135
Ashish0609 <ashishkanapuram@gmail.com>
2024-11-21 21:01:38 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,76 @@
+import java.util.Scanner;
+ 
 public class Main {
-  public static void main(String[] args) {
-    int[] myArray = {1, 5, 10, 25, 450, 250000, 6000000};
-    int sum = 0;
-    int i; 
-      
-    // Loop through array elements and get the sum
-    for (i = 0; i < myArray.length; i++) {
-      sum += myArray[i];
+ 
+    // Function that implements the
+    // number guessing game
+    public static void
+    guessingNumberGame()
+    {
+        // Scanner Class
+        Scanner sc = new Scanner(System.in);
+ 
+        // Generate the numbers
+        int number = 1 + (int)(100
+                               * Math.random());
+ 
+        // Given K trials
+        int K = 5;
+ 
+        int i, guess;
+ 
+        System.out.println(
+            "A number is chosen"
+            + " between 1 to 100."
+            + "Guess the number"
+            + " within 5 trials.");
+ 
+        // Iterate over K Trials
+        for (i = 0; i < K; i++) {
+ 
+            System.out.println(
+                "Guess the number:");
+ 
+            // Take input for guessing
+            guess = sc.nextInt();
+ 
+            // If the number is guessed
+            if (number == guess) {
+                System.out.println(
+                    "Congratulations!"
+                    + " You guessed the number.");
+                break;
+            }
+            else if (number > guess
+                     && i != K - 1) {
+                System.out.println(
+                    "The number is "
+                    + "greater than " + guess);
+            }
+            else if (number < guess
+                     && i != K - 1) {
+                System.out.println(
+                    "The number is"
+                    + " less than " + guess);
+            }
+        }
+ 
+        if (i == K) {
+            System.out.println(
+                "You have exhausted"
+                + " K trials.");
+ 
+            System.out.println(
+                "The number was " + number);
+        }
     }
-    System.out.println("The sum is: " + sum);
-  }
+ 
+    // Driver Code
+    public static void
+    main(String arg[])
+    {
+ 
+        // Function Call
+        guessingNumberGame();
+    }
 }
